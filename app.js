731c72b2f4522c65cb7ace09b778e325fbf3992f
Koval Andrii <andrijkoval30@gmail.com>
2022-10-07 18:12:02 +0300

    Revert "fix function names"
    This reverts commit adf3dc94680025dceb0008520fff3f2bb5524dda.

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-function shipPlacementGeneration()
+function createUserGrid()
 {
-    createGrid(getContainer("player-grid-container"), true);
-    createGrid(getContainer("computer-grid-container"), false);
-}
-
-function createGrid(container, isUser)
-{
+    const container = document.getElementById("player-grid-container");
     for(let i = 0; i < 10; i++)
     {
         for(let j = 0; j < 10; j++)
         {
             var item = document.createElement("div");
-            if(isUser)
-            {
-                item.id = "user";
-                item.id += " square-" + i + "-" + j;
-            }
-            else
-            {
-                item.id = "computer";
-                item.id += " square-" + i + "-" + j;
-            }
-            item.className += " square";
+            item.id = i + " " + j;
+            item.className = "square ";
+            //item.innerHTML = i + " " + j;
             container.appendChild(item);
         }
     }
 }
 
-function getContainer(id)
+function createComputerGrid()
 {
-    return document.getElementById(id);
+    const container = document.getElementById("computer-grid-container");
+    for(let i = 0; i < 10; i++)
+    {
+        for(let j = 0; j < 10; j++)
+        {
+            var item = document.createElement("div");
+            item.id = i + " " + j + " computer";
+            item.className = "square " + "square-shot ";
+            container.appendChild(item);
+        }
+    }
 }
 
-function getShipRandomPosition() 
+function getRandomStart()
 {
-    return { 
-        x : Math.floor(Math.random() * 10),
-        y : Math.floor(Math.random() * 10),
-        isHorizontal : isHorizontal = Math.random() > 0.5 
-    }
+    return Math.floor(Math.random() * 10);
+}
+
+function getRandomDirection()
+{
+    return Math.random() > 0.5;
 }
 
 function createShipCoordinates(squaresCount)
@@ -47,45 +45,48 @@ function createShipCoordinates(squaresCount)
     
     for(let ship = 0; ship < squaresCount.length; ship++)
     {
-        var position = getShipRandomPosition();
-    
-        if (position.isHorizontal)
+        var start_row = getRandomStart();
+        var start_col = getRandomStart();
+
+        var isHorizontal = getRandomDirection();
+        
+        if (isHorizontal)
         {
-            if(10 - squaresCount[ship] >= position.x)
+            if(10 - squaresCount[ship] >= start_col)
             {
                 for (let length = 0; length < squaresCount[ship]; length++)
                 {   
-                    shipCoordinates.push("ship-part-" + position.y + "-" + position.x);
-                    position.x++;
+                    shipCoordinates.push(start_row + " " + start_col);
+                    start_col++;
                 }
             }
             else
             {
-                position.x = 10 - squaresCount[ship];
+                start_col = 10 - squaresCount[ship];
                 for (let length = 0; length < squaresCount[ship]; length++)
                 {   
-                    shipCoordinates.push("ship-part-" + position.y + "-" + position.x);
-                    position.x++;
+                    shipCoordinates.push(start_row + " " + start_col);
+                    start_col++;
                 }
             }
         }
         else
         {
-            if(10 - squaresCount[ship] >= position.y)
+            if(10 - squaresCount[ship] >= start_row)
             {
                 for (let length = 0; length < squaresCount[ship]; length++)
                 {   
-                    shipCoordinates.push("ship-part-" + position.y + "-" + position.x);
-                    position.y++;
+                    shipCoordinates.push(start_row + " " + start_col);
+                    start_row++;
                 }
             }
             else
             {
-                position.y = 10 - squaresCount[ship];
+                start_row = 10 - squaresCount[ship];
                 for (let length = 0; length < squaresCount[ship]; length++)
                 {   
-                    shipCoordinates.push("ship-part-" + position.y + "-" + position.x);
-                    position.y++;
+                    shipCoordinates.push(start_row + " " + start_col);
+                    start_row++;
                 }
             }
         }
@@ -94,7 +95,7 @@ function createShipCoordinates(squaresCount)
     return shipCoordinates;
 }
 
-function createShipSquares(shipCoordinates, battleField)
+function createShipSquares(shipCoordinates, isUser)
 {
     for (let i = 0; i < shipCoordinates.length; i++) 
     {
@@ -102,20 +103,20 @@ function createShipSquares(shipCoordinates, battleField)
         {
             for(let y = 0; y < 10; y++)
             {
-                if (battleField == getContainer("player-grid-container"))
+                if (isUser)
                 {
-                    const square = document.getElementById("user square-" + x + "-" + y);
-                    if("ship-part-" + x + "-" + y === shipCoordinates[i])
+                    const square = document.getElementById(x + " " + y);
+                    if(square.id === shipCoordinates[i])
                     {
-                        square.className += " square-ship";
+                        square.className = "square " + "square-ship ";
                     }
                 }
-                else if (battleField == getContainer("computer-grid-container"))
+                else
                 {
-                    const square = document.getElementById("computer square-" + x + "-" + y);
-                    if("ship-part-" + x + "-" + y === shipCoordinates[i])
+                    const square = document.getElementById(x + " " + y + " computer");
+                    if(square.id === shipCoordinates[i] + " computer")
                     {
-                        square.className += " square-ship";
+                        square.className = "square " + "square-shot " + "square-ship ";
                     }
                 }
             }
@@ -126,24 +127,22 @@ function createShipSquares(shipCoordinates, battleField)
 function createUserShips()
 {
     var squaresCount = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+
     var shipCoordinates = createShipCoordinates(squaresCount);
-    var battleField = getContainer("player-grid-container");
-    createShipSquares(shipCoordinates, battleField);
+
+    createShipSquares(shipCoordinates, true);
 }
 
 function createComputerShips()
 {
     var squaresCount = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+
     var shipCoordinates = createShipCoordinates(squaresCount);
-    var battleField = getContainer("computer-grid-container");
-    createShipSquares(shipCoordinates, battleField);
+
+    createShipSquares(shipCoordinates, false);
 }
 
-function startSinglePlayerMode()
-{    
-    shipPlacementGeneration();
-    createUserShips();
-    createComputerShips();
-}
-
-startSinglePlayerMode();
+createUserGrid();
+createUserShips();
+createComputerGrid();
+createComputerShips();
